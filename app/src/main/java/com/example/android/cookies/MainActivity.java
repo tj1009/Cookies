f5420110e1b0608967c53bbd040ebfacc2f0eb5a
Tanya Jain <tjain.cse15@chitkara.edu.in>
2018-03-14 15:53:19 +0530

    first commit

---
+++ app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.cookies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void eatCookie(View view)
+    {
+        ImageView imageView = (ImageView) findViewById(R.id.android_cookie_image_view);
+        imageView.setImageResource(R.drawable.after_cookie);
+
+        TextView textView = (TextView) findViewById(R.id.status_text_view);
+        textView.setText("I'm so full");
+    }
+}
